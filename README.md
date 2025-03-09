5a4139b6cc8fed99f7dd4f65288b8c4aa258ec49
João Correia <joaocorreiaprof@gmail.com>
2025-03-09 03:10:34 +0000

    simple logic to add pizzas and orders

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Restaurant-App
+
 A simple Restaurant App built with TypeScript to practice and improve my TS skills.
+
+Work in progress...
---
+++ index.js
@@ -0,0 +1,40 @@
+var menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Peperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+];
+var cashInRegister = 100;
+var nextOrderId = 1;
+var orderQueue = [];
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj);
+}
+function placeOrder(pizzaName) {
+    var selectedPizza = menu.find(function (pizzaObj) { return pizzaObj.name === pizzaName; });
+    if (!selectedPizza) {
+        console.error("".concat(pizzaName, " does not exist"));
+        return;
+    }
+    cashInRegister += selectedPizza.price;
+    var newOrder = {
+        id: nextOrderId++,
+        pizza: selectedPizza,
+        status: "ordered",
+    };
+    orderQueue.push(newOrder);
+    return newOrder;
+}
+function completeOrder(orderId) {
+    var order = orderQueue.find(function (order) { return order.id === orderId; });
+    order.status === "completed";
+    return order;
+}
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 11 });
+placeOrder("Chicken Bacon Ranch");
+completeOrder(1);
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
