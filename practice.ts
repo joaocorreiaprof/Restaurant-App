47bcd6ae638b11e7ce82f2e8bf4f7b227c1ee4f6
João Correia <joaocorreiaprof@gmail.com>
2025-03-10 16:08:59 +0000

    refactor addNewPizza and addNewUser functions to return created objects; enhance updateUser function for better user management

--- practice.ts
+++ practice.ts
@@ -6,13 +6,34 @@ type User = {
   role: UserRole;
 };
 
+type UpdatedUser = Partial<User>;
+
+let nextUserId = 1;
+
 const users: User[] = [
-  { id: 1, username: "john_doe", role: "member" },
-  { id: 2, username: "jane_doe", role: "admin" },
-  { id: 3, username: "guest_user", role: "guest" },
+  { id: nextUserId++, username: "john_doe", role: "member" },
+  { id: nextUserId++, username: "jane_doe", role: "admin" },
+  { id: nextUserId++, username: "guest_user", role: "guest" },
 ];
 
-function updateUser(id: any, updates: any) {}
+function updateUser(id: number, updates: UpdatedUser) {
+  const foundUser = users.find((user) => user.id === id);
+  if (!foundUser) {
+    console.error("User not found");
+    return;
+  }
+  Object.assign(foundUser, updates);
+}
 
-updateUser(1, { username: "new_john_doe" });
-updateUser(3, { role: "admin" });
+function addNewUser(newUser: Omit<User, "id">): User {
+  const user: User = {
+    id: nextUserId++,
+    ...newUser,
+  };
+  users.push(user);
+  return user;
+}
+
+addNewUser({ username: "joe_schmoe", role: "member" });
+
+console.log(users);
